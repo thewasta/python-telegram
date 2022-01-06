57a5c6934d11181638559b63c1209283df64e249
schenier.lopez <6Jn=P*tfQP7dBt&w>
2022-01-06 14:56:05 +0100

    Improve download
    Included more channels from disney to download files

--- southpark.py
+++ southpark.py
@@ -40,35 +40,75 @@ async def get_chats_data():
 
 
 async def main():
-    counter = 0
+    counter_season = 30
     for channel in channels_videos:
-        counter += 1
+        counter = 1
         peer_channel = PeerChannel(int("-{}".format(channel)))
+        entity = await client.get_entity(peer_channel)
+        channel_name = " ".join(entity.title.split(" ")[:2])
+        print("------ CHANNEL: {}".format(entity))
         async for dialog in client.iter_messages(entity=peer_channel):
-            entity = await client.get_entity(peer_channel)
-            channel_name = " ".join(entity.title.split(" ")[:2])
-            if dialog.media:
-                if re.search("mp4", dialog.media.document.mime_type):
-                    if hasattr(dialog.media.document.attributes[0], 'file_name'):
-                        dialog_file_name = dialog.media.document.attributes[0].file_name
-                        await download_media(channel_name, dialog, dialog_file_name)
-                    else:
-                        dialog_file_name = channel_name.replace(" ", "") + "-unknown-" + str(counter) + ".mp4"
-                        await download_media(channel_name, dialog, dialog_file_name)
+            if dialog.media and not hasattr(dialog.media, "photo"):
+                if re.search("video", dialog.media.document.mime_type) or \
+                        re.search(".rar", dialog.media.document.mime_type):
+                    await download_media(channel_name, dialog, counter_season, counter)
             counter += 1
+        counter_season += 1
+
     exit()
 
 
-async def download_media(channel_name, dialog, dialog_file_name):
-    abs_path = os.path.join(Path.cwd(), channel_name, dialog_file_name)
-    path_to_file = Path(abs_path)
-    if not os.path.exists(path_to_file.parent):
-        os.mkdir(channel_name)
-    await dialog.download_media(path_to_file)
+async def download_media(channel_name, dialog, season, chapter):
+    dialog_message_id = dialog.media.document.id
+    if not already_downloaded(dialog_message_id):
+        mime_type = dialog.media.document.mime_type
+        if dialog.message != "":
+            file_name = dialog.message + "." + mime_type.split("/")[1]
+        elif hasattr(dialog.media.document.attributes[0], "file_name"):
+            file_name = dialog.media.document.attributes[0].file_name
+        else:
+            file_name = "Unknown-{}-{}.{}".format(season, chapter, mime_type.split("/")[1])
+        if channel_name != "South Park":
+            tv_show_media_mime = file_name[-4:]
+            tv_show_with_chapter = file_name.split("@")[0]
+            tv_show_chapter = re.match("[0-9]{1,2}(x)[0-9]{1,2}", tv_show_with_chapter).group()
+            tv_show_name = tv_show_with_chapter.replace(tv_show_chapter, "").replace("-", "")
+            if re.match("^[0-9]", tv_show_name):
+                print(tv_show_name.split(" ")[1:])
+                tv_show_name = " ".join(tv_show_name.split(" ")[1:])
+                print(tv_show_name)
+            abs_path = os.path.join(config["Telegram"]["folder"], "TV Shows", tv_show_name.strip(),
+                                    tv_show_chapter.strip() + tv_show_media_mime)
+        else:
+            abs_path = os.path.join(config["Telegram"]["folder"], "TV Shows", channel_name, file_name)
+        path_to_file = Path(abs_path)
+        if not os.path.exists(path_to_file.parent.parent):
+            os.mkdir(path_to_file.parent.parent)
+        if not os.path.exists(path_to_file.parent):
+            os.mkdir(path_to_file.parent)
+        with open("downloads.txt", "a") as file:
+            file.write(str(dialog_message_id) + " " + str(path_to_file))
+            file.write("\n")
+        # await dialog.download_media(path_to_file)
 
 
-with client:
-    client.loop.run_until_complete(main())
+def already_downloaded(media_id):
+    result = False
+    with open("downloads.txt") as temp_file:
+        data_file = temp_file.readlines()
+        for line in data_file:
+            if str(media_id) in line:
+                result = True
+    return result
 
-client.start()
-client.run_until_disconnected()
+
+def bot():
+    with client:
+        client.loop.run_until_complete(main())
+
+    client.start()
+    client.run_until_disconnected()
+
+
+if __name__ == "__main__":
+    bot()
